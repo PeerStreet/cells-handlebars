d30f63c78def52e2439d60c9c35267168604478b
Evan Petrie <evan@peerstreet.com>
2015-08-24 11:30:43 -0700

    handle escaping properly

--- lib/tilt/handlebars_template.rb
+++ lib/tilt/handlebars_template.rb
@@ -5,7 +5,9 @@ module Tilt
     end
 
     def evaluate(scope, locals, &block)
-      @data.gsub(/\{{2,3}(.*?)\}{2,3}/){|_| scope.send $1.to_sym }
+      @data.gsub!(/\{{3}(.*?)\}{3}/){|_| scope.send $1.to_sym }
+      @data.gsub!(/\{{2}(.*?)\}{2}/){|_| CGI::escapeHTML scope.send($1.to_sym).to_s }
+      @data
     end
 
   end
