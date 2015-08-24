53113224e618298d5707bda9425850c5507e5939
Evan Petrie <evan@peerstreet.com>
2015-08-24 11:56:59 -0700

    don't ! sub

--- lib/tilt/handlebars_template.rb
+++ lib/tilt/handlebars_template.rb
@@ -5,9 +5,7 @@ module Tilt
     end
 
     def evaluate(scope, locals, &block)
-      @data.gsub!(/\{{3}(.*?)\}{3}/){|_| scope.send $1.to_sym }
-      @data.gsub!(/\{{2}(.*?)\}{2}/){|_| CGI::escapeHTML scope.send($1.to_sym).to_s }
-      @data
+      @data.gsub(/\{{3}(.*?)\}{3}/){|_| scope.send $1.to_sym }.gsub(/\{{2}(.*?)\}{2}/){|_| CGI::escapeHTML scope.send($1.to_sym).to_s }
     end
 
   end
